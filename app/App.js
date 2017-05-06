8deef25c4a71d62c430e3864caa9796b5a45d2e9
Tessa Siegel <t.e.siegel@gmail.com>
2017-05-05 19:59:51 -0700

    Scaffolds out basic React app with webpack

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World ReactJS!!!
+         </div>
+      );
+   }
+}
+export default App;
